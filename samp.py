0ea48847467c49d5577cb7b4eb42bc73b2e1cf6d
lavi-wr <4475lavanyaagrawal@gmail.com>
2025-09-19 16:37:07 +0530

    changes

--- samp.py
+++ samp.py
@@ -1,10 +1,34 @@
+import os
+from dotenv import load_dotenv
 import google.generativeai as genai
+import pyttsx3
+load_dotenv()
 
-genai.configure(api_key="AIzaSyDiYFvfGtswXQ0hthizIz72kOkYxYx_ics")
+def speak(text):
+    # Initialize engine
+    engine = pyttsx3.init()
+    
+    # Set properties (optional)
+    engine.setProperty("rate", 150)   # Speed (words per minute)
+    engine.setProperty("volume", 1.0)  # Volume (0.0 to 1.0)
+    
+    # Speak the text
+    engine.say(text)
+    engine.runAndWait()
+
+
+key = os.getenv("API_KEY")
+
+genai.configure(api_key=key)
 
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 
-response = model.generate_content("make a shayri.")
-print(response.text)
+response = model.generate_content("make a poem")
+speak(response.text)
+
+
+
+
+
