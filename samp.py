19f15e5462ddf4ee41637368570c4bf3d054b07e
me-is-mukul <me.is.mukul.2005@gmail.com>
2025-09-20 22:43:09 +0530

    changes

--- samp.py
+++ samp.py
@@ -2,17 +2,17 @@ import os
 from dotenv import load_dotenv
 import google.generativeai as genai
 import pyttsx3
+import speech_recognition as sr
 load_dotenv()
 
+r = sr.Recognizer()
+
 def speak(text):
-    # Initialize engine
     engine = pyttsx3.init()
-    
-    # Set properties (optional)
-    engine.setProperty("rate", 150)   # Speed (words per minute)
-    engine.setProperty("volume", 1.0)  # Volume (0.0 to 1.0)
-    
-    # Speak the text
+
+    engine.setProperty("rate", 150)   
+    engine.setProperty("volume", 1.0) 
+
     engine.say(text)
     engine.runAndWait()
 
@@ -25,10 +25,27 @@ genai.configure(api_key=key)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 
-response = model.generate_content("make a poem")
-speak(response.text)
+# response = model.generate_content("make a poem")
+# speak(response.text)
+
+# with sr.Microphone() as source:
+#     print("🎙️ Speak something...")
+#     #r.adjust_for_ambient_noise(source)   # optional (reduces background noise)
+#     audio = r.listen(source, timeout=5, phrase_time_limit=10)
+
+# try:
+#     # Recognize using Google Speech Recognition (free, online)
+#     text = r.recognize_google(audio)
+#     print("You said:", text)
+
+# except sr.UnknownValueError:
+#     print("Sorry, I could not understand your speech.")
+# except sr.RequestError:
+#     print("Could not request results, check your internet connection.")
 
 
+
+print(sr.Microphone.list_microphone_names())
 
 
 
